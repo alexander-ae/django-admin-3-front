98394621fbcec9df7c583b4f232e99b594e11541
danielhuamani <danielhuamani15@gmail.com>
2016-07-10 15:25:48 -0500

    modificando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,9 @@ const gulp = require('gulp'),
 const path = {
   pug: "src/templates/pug/**/*.pug",
   html: 'src/templates/html',
-  stylus_block: 'src/static/stylus/block/*.styl',
-  stylus_themes: 'src/static/stylus/themes/*.styl',
-  stylus_base: 'src/static/stylus/base/*.styl',
-  stylus_pages: 'src/static/stylus/pages/*.styl',
+  stylus_blocks: 'src/static/stylus/**/*.styl',
   stylus: 'src/static/stylus/*.styl',
   css: 'src/static/css',
-  css_themes: 'src/static/css/themes',
-  css_pages: 'src/static/css/pages',
-  css_base: 'src/static/css/base',
-  css_block: 'src/static/css/block',
 }
 
 // Compilando pug a html
@@ -53,63 +46,61 @@ gulp.task('stylus', () => {
 });
 
 
-gulp.task('stylus_block', () => {
-  return gulp.src(path.stylus_block)
+gulp.task('stylus_blocks', () => {
+  return gulp.src(path.stylus_blocks)
     .pipe(plumber())
     .pipe(stylus({
       use: nib()
     }))
-    .pipe(gulp.dest(path.css_block))
+    .pipe(gulp.dest(path.css))
     .pipe(connect.reload())
 });
 
 
-gulp.task('stylus_pages', () => {
-  return gulp.src(path.stylus_pages)
-    .pipe(plumber())
-    .pipe(stylus({
-      use: nib()
-    }))
-    .pipe(gulp.dest(path.css_pages))
-    .pipe(connect.reload())
-});
+// gulp.task('stylus_pages', () => {
+//   return gulp.src(path.stylus_pages)
+//     .pipe(plumber())
+//     .pipe(stylus({
+//       use: nib()
+//     }))
+//     .pipe(gulp.dest(path.css_pages))
+//     .pipe(connect.reload())
+// });
 
 
-gulp.task('stylus_themes', () => {
-  return gulp.src(path.stylus_themes)
-    .pipe(plumber())
-    .pipe(stylus({
-      use: nib()
-    }))
-    .pipe(gulp.dest(path.css_themes))
-    .pipe(connect.reload())
-});
+// gulp.task('stylus_themes', () => {
+//   return gulp.src(path.stylus_themes)
+//     .pipe(plumber())
+//     .pipe(stylus({
+//       use: nib()
+//     }))
+//     .pipe(gulp.dest(path.css_themes))
+//     .pipe(connect.reload())
+// });
 
-gulp.task('stylus_base', () => {
-  return gulp.src(path.stylus_base)
-    .pipe(plumber())
-    .pipe(stylus({
-      use: nib()
-    }))
-    .pipe(gulp.dest(path.css_base))
-    .pipe(connect.reload())
-});
+// gulp.task('stylus_base', () => {
+//   return gulp.src(path.stylus_base)
+//     .pipe(plumber())
+//     .pipe(stylus({
+//       use: nib()
+//     }))
+//     .pipe(gulp.dest(path.css_base))
+//     .pipe(connect.reload())
+// });
 
 
 // funcion donde observara gulp
 gulp.task('watch', () => {
   gulp.watch(path.pug, ['pug'])
   gulp.watch(path.stylus, ['stylus'])
-  gulp.watch(path.stylus, ['stylus_block'])
-  gulp.watch(path.stylus, ['stylus_themes'])
-  gulp.watch(path.base, ['stylus_base'])
-  gulp.watch(path.pages, ['stylus_pages'])
+  gulp.watch(path.stylus, ['stylus_blocks'])
+
 });
 
 // Creando servidor con livereload
 gulp.task('connect', () => {
   connect.server({
-    root: 'app/',
+    root: 'src/',
     hostname: '0.0.0.0',
     livereload: true,
     port: '3000',
@@ -124,4 +115,4 @@ gulp.task('connect', () => {
 // Ejecutando gulp
 
 // por Default
-gulp.task('default', ['pug', 'watch', 'connect', 'stylus', 'stylus_block', 'stylus_themes', 'stylus_base', 'stylus_pages']);
+gulp.task('default', ['pug', 'watch', 'connect', 'stylus', 'stylus_blocks']);
