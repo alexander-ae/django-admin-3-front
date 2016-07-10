a90a557aa32f691b3f46d2bedf839926f48514f6
danielhuamani <danielhuamani15@gmail.com>
2016-07-10 00:10:37 -0500

    diviendo stylus y css, agregando carpetas pages block themes y base

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 'use strict';
 
 const gulp = require('gulp'),
-	jade = require('gulp-jade'),
-	sass = require('gulp-sass'),
-	connect = require('gulp-connect'),
-	plumber = require('gulp-plumber'),
-	autoprefixer = require('gulp-autoprefixer'),
-	historyApiFallback = require('connect-history-api-fallback')
+  jade = require('gulp-jade'),
+  stylus = require('gulp-stylus'),
+  nib = require('nib'),
+  connect = require('gulp-connect'),
+  plumber = require('gulp-plumber'),
+  historyApiFallback = require('connect-history-api-fallback')
 
 
 // Rutas de las carpetas
 const path = {
-	jade: "src/templates/jade/**/*.jade",
-	html: 'src/templates/html'
-
+  jade: "src/templates/jade/**/*.jade",
+  html: 'src/templates/html',
+  stylus_block: 'src/static/stylus/block/*.styl',
+  stylus_themes: 'src/static/stylus/themes/*.styl',
+  stylus_base: 'src/static/stylus/base/*.styl',
+  stylus_pages: 'src/static/stylus/pages/*.styl',
+  stylus: 'src/static/stylus/*.styl',
+  css: 'src/static/css',
+  css_block: 'src/static/css/themes',
+  css_block: 'src/static/css/pages',
+  css_block: 'src/static/css/base',
 }
 
-// Compilando Jade
+// Compilando Jade a html
 
 gulp.task('jade', () => {
 
@@ -31,9 +39,70 @@ gulp.task('jade', () => {
 
 });
 
+
+// Stylus
+gulp.task('stylus', () => {
+  return gulp.src(path.stylus)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(gulp.dest(path.css))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('stylus_block', () => {
+  return gulp.src(path.stylus_block)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(gulp.dest(path.css_block))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('stylus_pages', () => {
+  return gulp.src(path.stylus_pages)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(gulp.dest(path.css_pages))
+    .pipe(connect.reload())
+});
+
+
+gulp.task('stylus_themes', () => {
+  return gulp.src(path.stylus_themes)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(gulp.dest(path.css_themes))
+    .pipe(connect.reload())
+});
+
+gulp.task('stylus_base', () => {
+  return gulp.src(path.stylus_base)
+    .pipe(plumber())
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(gulp.dest(path.css_base))
+    .pipe(connect.reload())
+});
+
+
 // funcion donde observara gulp
 gulp.task('watch', () => {
-	gulp.watch(path.jade, ['jade'])
+  gulp.watch(path.jade, ['jade'])
+  gulp.watch(path.stylus, ['stylus'])
+  gulp.watch(path.stylus, ['stylus_block'])
+  gulp.watch(path.stylus, ['stylus_themes'])
+  gulp.watch(path.base, ['stylus_base'])
+  gulp.watch(path.pages, ['stylus_pages'])
 });
 
 // Creando servidor con livereload
@@ -54,4 +123,4 @@ gulp.task('connect', () => {
 // Ejecutando gulp
 
 // por Default
-gulp.task('default', ['jade', 'watch', 'connect']);
+gulp.task('default', ['jade', 'watch', 'connect', 'stylus', 'stylus_block', 'stylus_themes', 'stylus_base', 'stylus_pages']);
